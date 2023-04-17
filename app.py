d58f45ff55c778a268f9449a297a69283b8711eb
Your Name <you@example.com>
2023-04-17 14:20:37 +0530

    x

--- app.py
+++ app.py
@@ -1,19 +1,44 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from datetime import date
+DEFAULT_PASSWORD = 'admin'
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+# # Dummy data for book categories and list of books
+
+import string
+import random
+
 # Dummy data for book categories and list of books
 categories = ['Hospitality', 'Civil', 'Chemistry', 'Physics', 'Dictionaries', 'Mechanical', 'Fashion Design', 'Misc', 'Law', 'Electronics'] 
 books = {
         'Hospitality': [], 'Civil':[], 'Chemistry':[], 'Physics':[], 'Dictionaries':[], 'Mechanical':[], 'Fashion Design':[], 'Misc':[], 'Law':[], 'Electronics':[]
 }
 
+# Function to generate random book names
+def generate_book_name():
+    # Generate a random string of length between 5 to 15 characters
+    length = random.randint(5, 15)
+    return ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
+
+# Populate the books dictionary with random book names
+for category in categories:
+    for i in range(10):  # Generate 10 books per category
+        book_name = generate_book_name()
+        books[category].append(book_name)
+
+# categories = ['Hospitality', 'Civil', 'Chemistry', 'Physics', 'Dictionaries', 'Mechanical', 'Fashion Design', 'Misc', 'Law', 'Electronics'] 
+# books = {
+#         'Hospitality': [], 'Civil':[], 'Chemistry':[], 'Physics':[], 'Dictionaries':[], 'Mechanical':[], 'Fashion Design':[], 'Misc':[], 'Law':[], 'Electronics':[]
+# }
+
+# Default admin username and password
+default_username = 'admin'
+default_password = 'password'
+
 # Route for index page
 @app.route('/')
 def index():
-    if 'username' not in session:
-        return redirect(url_for('signup'))
     return render_template('index.html', date=date)
 
 #Route for search functionality
@@ -30,8 +55,8 @@ def search():
 # Route for list page
 @app.route('/list', methods=['GET', 'POST'])
 def book_list():
-    if 'username' not in session:
-        return redirect(url_for('signup'))
+    if session.get('username') != default_username:
+        return redirect(url_for('login'))
     if request.method == 'POST':
         category = request.form['category']
         book_name = request.form['book_name']
@@ -44,32 +69,26 @@ def book_list():
         selected_category = request.args.get('category', categories[0])
         return render_template('list.html', categories=categories, selected_category=selected_category, book_list=books[selected_category], date=date)
 
-# Route for signup page
-# @app.route('/signup', methods=['GET', 'POST'])
-# def signup():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         # TODO: Implement logic for signing up with a username
-#         return redirect(url_for('index'))
-#     else:
-#         return render_template('signup.html', date=date)
-# Route for signup page
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
         username = request.form['username']
-        if username not in session:
+        password = request.form['password']
+        if username == 'admin' and password == DEFAULT_PASSWORD:
             session['username'] = username
             return redirect(url_for('index'))
         else:
-            return render_template('signup.html', date=date, error='Username already taken')
+            return render_template('login.html', date=date, error='Invalid username or password')
     else:
-        return render_template('signup.html', date=date)
+        return render_template('login.html', date=date)
+
 
 @app.route('/logout')
 def logout():
-    if 'username' not in session:
-        return redirect(url_for('signup'))
+    if session.get('username') != default_username:
+        return redirect(url_for('login'))
     session.pop('username', None)
     return redirect(url_for('index'))
 
