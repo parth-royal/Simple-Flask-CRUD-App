0e01caae83191ef5fff96c419f53cea667dd7b36
emmi-01 <119301270+emmi-01@users.noreply.github.com>
2023-06-10 12:03:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,98 +1,51 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from datetime import date
-DEFAULT_PASSWORD = 'admin'
+import uuid
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-# # Dummy data for book categories and list of books
+app.secret_key = 'admin123'
 
-import string
-import random
+users = []
 
-# Dummy data for book categories and list of books
-categories = ['Hospitality', 'Civil', 'Chemistry', 'Physics', 'Dictionaries', 'Mechanical', 'Fashion Design', 'Misc', 'Law', 'Electronics'] 
-books = {
-        'Hospitality': [], 'Civil':[], 'Chemistry':[], 'Physics':[], 'Dictionaries':[], 'Mechanical':[], 'Fashion Design':[], 'Misc':[], 'Law':[], 'Electronics':[]
-}
-
-# Function to generate random book names
-def generate_book_name():
-    # Generate a random string of length between 5 to 15 characters
-    length = random.randint(5, 15)
-    return ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
-
-# Populate the books dictionary with random book names
-for category in categories:
-    for i in range(10):  # Generate 10 books per category
-        book_name = generate_book_name()
-        books[category].append(book_name)
-
-# categories = ['Hospitality', 'Civil', 'Chemistry', 'Physics', 'Dictionaries', 'Mechanical', 'Fashion Design', 'Misc', 'Law', 'Electronics'] 
-# books = {
-#         'Hospitality': [], 'Civil':[], 'Chemistry':[], 'Physics':[], 'Dictionaries':[], 'Mechanical':[], 'Fashion Design':[], 'Misc':[], 'Law':[], 'Electronics':[]
-# }
-
-# Default admin username and password
-default_username = 'admin'
-default_password = 'password'
-
-# Route for index page
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', date=date)
+    return render_template("index.html", datas=users)
 
-#Route for search functionality
-@app.route('/search')
-def search():
-    query = request.args.get('q')
-    results = []
-    for category in categories:
-        for book in books[category]:
-            if query.lower() in book.lower():
-                results.append((book, category))
-    return results
-
-# Route for list page
-@app.route('/list', methods=['GET', 'POST'])
-def book_list():
-    if session.get('username') != default_username:
-        return redirect(url_for('login'))
+@app.route("/add_user", methods=['POST', 'GET'])
+def add_user():
     if request.method == 'POST':
-        category = request.form['category']
-        book_name = request.form['book_name']
-        if category not in categories:
-            categories.append(category)
-            books[category] = []
-        books[category].append(book_name)
-        return redirect(url_for('book_list', category=category))
-    else:
-        selected_category = request.args.get('category', categories[0])
-        return render_template('list.html', categories=categories, selected_category=selected_category, book_list=books[selected_category], date=date)
+        uname = request.form['uname']
+        contact = request.form['contact']
+        user = {'uid': str(uuid.uuid4()), 'uname': uname, 'contact': contact}
+        users.append(user)
+        flash('User Added', 'success')
+        return redirect(url_for("index"))
+    return render_template("add_user.html")
 
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route("/edit_user/<string:uid>", methods=['POST', 'GET'])
+def edit_user(uid):
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username == 'admin' and password == DEFAULT_PASSWORD:
-            session['username'] = username
-            return redirect(url_for('index'))
-        else:
-            return render_template('login.html', date=date, error='Invalid username or password')
+        uname = request.form['uname']
+        contact = request.form['contact']
+        for user in users:
+            if user['uid'] == uid:
+                user['uname'] = uname
+                user['contact'] = contact
+                flash('User Updated', 'success')
+                return redirect(url_for("index"))
     else:
-        return render_template('login.html', date=date)
+        for user in users:
+            if user['uid'] == uid:
+                return render_template("edit_user.html", datas=user)
+    return redirect(url_for("index"))
 
+@app.route("/delete_user/<string:uid>", methods=['GET'])
+def delete_user(uid):
+    for user in users:
+        if user['uid'] == uid:
+            users.remove(user)
+            flash('User Deleted', 'warning')
+            break
+    return redirect(url_for("index"))
 
-@app.route('/logout')
-def logout():
-    if session.get('username') != default_username:
-        return redirect(url_for('login'))
-    session.pop('username', None)
-    return redirect(url_for('index'))
-
-
-# Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
